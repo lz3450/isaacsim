583113e97cba123728e3e56903a48dbfd6c91f26
KZL <lz3450@outlook.com>
2025-04-29 13:19:14 -0500

    python setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 if [ -n "$BASH_VERSION" ]; then
-  export ISAAC_SIM_ROOT_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1; pwd -P)"
+  export ISAAC_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1; pwd -P)"
 elif [ -n "$ZSH_VERSION" ]; then
-  export ISAAC_SIM_ROOT_DIR="$(cd -- "$(dirname "${(%):-%x}")" > /dev/null 2>&1; pwd -P)"
+  export ISAAC_DIR="$(cd -- "$(dirname "${(%):-%x}")" > /dev/null 2>&1; pwd -P)"
 else
   echo "Unsupported shell"
 fi
 
-echo "ISAAC_SIM_ROOT_DIR=$ISAAC_SIM_ROOT_DIR"
-export PATH="$ISAAC_SIM_ROOT_DIR:$PATH"
+. ../RoboTrace/moveit2_ws/moveit2_setup.sh
+echo "ISAAC_DIR=$ISAAC_DIR"
+export PATH="$ISAAC_DIR:$ISAAC_DIR/kit/python/bin:$PATH"
