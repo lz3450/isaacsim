4bbd177da1d958a6b5981af46f77360d5aade911
KZL <lz3450@outlook.com>
2025-08-21 01:50:57 -0500

    rg: tests

---
+++ roboguard/tests/test1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python.sh
+#
+# test1.py
+#
+# Test for standalone Isaac Sim extension loading
+#
+
+from isaacsim.simulation_app import SimulationApp
+
+simulation_app = SimulationApp({"headless": False})
+
+EXTENSIONS = [
+    "isaacsim.ros2.bridge",
+    "omni.graph.window.action",
+    "omni.kit.window.script_editor",
+    "isaacsim.code_editor.vscode",
+    "isaacsim.code_editor.jupyter",
+]
+
+from isaacsim.core.version import get_version
+from isaacsim.core.utils.extensions import enable_extension
+
+print("Isaac Sim version: ", get_version())
+
+for e in EXTENSIONS:
+    enable_extension(e)
+    simulation_app.update()
+
+while simulation_app.is_running():
+    simulation_app.update()
+simulation_app.close()
