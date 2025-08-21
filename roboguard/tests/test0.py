3577d46ff232e01a4833018217d1416ebc79ac51
KZL <lz3450@outlook.com>
2025-08-21 01:11:05 -0500

    rg: init

---
+++ roboguard/tests/test0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python.sh
+#
+# test0.py
+#
+# Test for standalone Isaac Sim
+#
+
+from isaacsim.simulation_app import SimulationApp
+
+simulation_app = SimulationApp({"headless": True})
+
+from isaacsim.core.version import get_version
+
+print("Isaac Sim version: ", get_version())
