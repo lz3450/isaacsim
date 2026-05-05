a80f333be27df43072f1b122474e54595b56c3a0
KZL <lz3450@outlook.com>
2026-05-05 01:46:53 -0500

    ri: carter standalone app

---
+++ roboinsight/carter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python.sh
+#
+# carter.py
+#
+
+from isaacsim.simulation_app import SimulationApp
+
+simulation_app = SimulationApp({"headless": False})
+
+from isaacsim.core.utils.extensions import enable_extension
+from isaacsim.core.utils.nucleus import get_assets_root_path
+from isaacsim.core.utils.stage import open_stage
+from isaacsim.core.api import World
+
+EXTENSIONS = [
+    "isaacsim.ros2.bridge",
+    "omni.graph.window.action",
+    "omni.kit.window.script_editor",
+    "isaacsim.code_editor.vscode",
+    "isaacsim.code_editor.jupyter",
+]
+
+for e in EXTENSIONS:
+    enable_extension(e)
+    simulation_app.update()
+
+assets_root = get_assets_root_path()
+if assets_root is None:
+    raise RuntimeError("Could not find Isaac Sim assets root")
+
+stage_path = assets_root + "/Isaac/Samples/ROS2/Scenario/carter_warehouse_navigation.usd"
+
+print("Loading stage:", stage_path)
+
+open_stage(stage_path)
+
+world = World()
+world.reset()
+
+while simulation_app.is_running():
+    world.step(render=True)
+
+simulation_app.close()
