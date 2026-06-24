aa9644b6a27b7edddc7dc4f1cb9b1002ec7abd18
KZL <lz3450@outlook.com>
2026-06-24 11:16:47 -0500

    scripts: ri

--- isaac-sim.ri.sh
+++ isaac-sim.ri.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 export RESOURCE_NAME="IsaacSim"
 export OLD_PYTHONPATH=$PYTHONPATH
 
-. "$SCRIPT_DIR/setup_ri_env.sh"
+unset SCRIPT_DIR
+. ./setup_ri_env.sh
 
-exec "$SCRIPT_DIR/kit/kit" "$SCRIPT_DIR/apps/isaacsim.exp.selector.kit" "$@"
+exec "$SCRIPT_DIR/kit/kit" "$SCRIPT_DIR/apps/isaacsim.exp.full.kit" "$@"
